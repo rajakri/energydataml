6cf8bc60a30d2f8e0f9f21653ddc482b9b404664
Raja Krishnan <rajakri@gmail.com>
2014-09-13 12:53:52 +1000

    [Raja]:Created the intial Shell Maven Project using Apache Mahout

---
+++ energydata/src/main/java/au/com/iag/energydata/App.java
@@ -0,0 +1,16 @@
+package au.com.iag.energydata;
+
+import java.io.File;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    }
+}
