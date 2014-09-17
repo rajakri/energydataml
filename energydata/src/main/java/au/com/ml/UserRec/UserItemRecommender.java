94b5a61fd6d4a411b3fd30fe797a8d1ef592f9e4
Raja Krishnan <rajakri@gmail.com>
2014-09-17 21:21:43 +1000

    [Raja] Changed to useritem based Recommender since due to lack of time

--- energydata/src/main/java/au/com/ml/UserRec/UserItemRecommender.java
+++ energydata/src/main/java/au/com/ml/UserRec/UserItemRecommender.java
@@ -1,28 +1,30 @@
 /**
  * Sample Recommender user-based recommender
  */
-package au.com.ml.energydata;
+package au.com.ml.UserRec;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.CachingRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
-import org.apache.mahout.cf.taste.common.TasteException;
-import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
-import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
-import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
-import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+
 
 /**
  * @author u321146
  *
  */
-public class FridgeRecommender {
+public class UserItemRecommender {
 
 	/**
 	 * @param args
@@ -30,12 +32,17 @@ public class FridgeRecommender {
 	 * @throws TasteException 
 	 */
 	public static void main(String[] args) throws IOException, TasteException {
-		//dataset is of the format userID,itemID,value
-		DataModel model = new FileDataModel(new File("data/dataset.csv"));
+		DataModel model = createModel();
+		//for large data sets item based recommender is appropriate
+        // create a simple recommender on our data
+       
+        
 		UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
 		UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
 		UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
 		
+		CachingRecommender cachingRecommender = new CachingRecommender(recommender);
+		 
 		//Recommender cachingRecommender = new CachingRecommender(recommender);
 		//Userid and Number of recommendations
 		List<RecommendedItem> recommendations = recommender.recommend(2, 3);
@@ -47,6 +54,13 @@ public class FridgeRecommender {
 		}
 		
 		
+	}
+
+	private static DataModel createModel() throws IOException {
+		//Load Data and create model
+		//DataModel model = new FileDataModel(new File("data/FF2014_09_12.csv"));
+		DataModel model = new FileDataModel(new File("data/dataset.csv"));
+		return model;
 	}
 
 }
